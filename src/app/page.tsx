f9e7f19bdcec318940f6e8c95cd50bea4b84338c
Alan Rocha <alanrochaarg2001@gmail.com>
2024-11-06 19:32:47 -0300

    fix(page): replace - with camelCase at SVG inside tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import HeroSection from "@/components/hero-section";
 
 export default function Home() {
@@ -70,9 +71,9 @@ export default function Home() {
               fill="currentColor"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             Menu
